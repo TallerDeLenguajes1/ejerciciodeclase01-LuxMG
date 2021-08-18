ae33f8d0373b573eaae48925b46fd16ce0ac252f
luchechita90@outlook.com <luchechita90@outlook.com>
2021-08-18 18:11:31 -0300

    Subido el ejercicio pedido en clase (Adivinar un numero aleatorio)

---
+++ EjercicioClase01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EjercicioClase01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adivine el numero aleatorio.");
+
+            bool encontrado = false;
+            int aleatorio = new Random().Next(101); //numero entre 0 y 100
+            int num = 0;
+
+            do
+            {
+                Console.Write("\nIngrese un numero: ");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num > aleatorio)
+                {
+                    Console.WriteLine(num + " > aleatorio");
+                }
+                else if (num < aleatorio)
+                {
+                    Console.WriteLine(num + " < aleatorio");
+                }
+                else
+                {
+                    Console.WriteLine("Felicidades! Encontró el numero!");
+                    encontrado = true;
+                }
+
+            } while (!encontrado);
+        }
+    }
+}
