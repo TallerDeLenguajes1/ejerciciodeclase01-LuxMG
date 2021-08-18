03c30576220ffcdcf8f44b7f645289a5878391d6
luchechita90@outlook.com <luchechita90@outlook.com>
2021-08-18 19:09:33 -0300

    Mejorado un poco

--- EjercicioClase01/Program.cs
+++ EjercicioClase01/Program.cs
@@ -10,28 +10,39 @@ namespace EjercicioClase01
 
             bool encontrado = false;
             int aleatorio = new Random().Next(101); //numero entre 0 y 100
-            int num = 0;
+            int num = 0, cont = 0;
 
             do
             {
-                Console.Write("\nIngrese un numero: ");
-                num = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    Console.Write("\nIngrese un numero: ");
+                    if(int.TryParse(Console.ReadLine(), out num))
+                    {
+                        if (num > 100 || num < 0)
+                            Console.WriteLine("Solo se aceptan numeros entre 0 y 100");
+                        else if (num > aleatorio)
+                            Console.WriteLine(num + " > aleatorio");
+                        else if (num < aleatorio)
+                            Console.WriteLine(num + " < aleatorio");
+                        else
+                            encontrado = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Recuerde solo utilizar numeros");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ocurrio un error en la ejecución.");
+                }
 
-                if (num > aleatorio)
-                {
-                    Console.WriteLine(num + " > aleatorio");
-                }
-                else if (num < aleatorio)
-                {
-                    Console.WriteLine(num + " < aleatorio");
-                }
-                else
-                {
-                    Console.WriteLine("Felicidades! Encontró el numero!");
-                    encontrado = true;
-                }
+                cont++;
 
             } while (!encontrado);
+
+            Console.WriteLine("Felicidades! Encontró el numero " + aleatorio + " en " + cont + " intentos!");
         }
     }
 }
